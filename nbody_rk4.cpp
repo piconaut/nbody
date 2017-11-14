bb8765e9f66ab76eca5639d6f4a046b8e8313a8e
ianfare <apellius@gmail.com>
2017-11-13 20:48:03 -0500

    Fixed printing

--- nbody_rk4.cpp
+++ nbody_rk4.cpp
@@ -181,8 +181,8 @@ int main()
  //     velocity_sq = pow(velocities[star][0],2) + pow(velocities[star][1],2) + pow(velocities[star][2],2);
  //     Et += 0.5 * masses[star] * velocity_sq;
       cout << masses[star] << " " << positions[star][0] << " " 
-           << positions[star][1] << " " << positions[star][2]
-           << velocities[star][0] << " " << velocities[star][1]
+           << positions[star][1] << " " << positions[star][2] << " "
+           << velocities[star][0] << " " << velocities[star][1] << " "
            << velocities[star][2] << endl;
     }
 //    cout << Et << endl;
