8e5d85ac60ba8efad14bcea2f61a6bf4aac89d7a
ianfare <apellius@gmail.com>
2017-11-16 03:14:47 -0500

    Comments

--- nbody_rk4.cpp
+++ nbody_rk4.cpp
@@ -6,6 +6,7 @@
 
 using namespace std;
 
+// Vector addition
 vector<double> vectoradd(vector<double> vec1, vector<double> vec2)
 {
   int vecsize = vec1.size();
@@ -16,6 +17,7 @@ vector<double> vectoradd(vector<double> vec1, vector<double> vec2)
   return sum;
 }
 
+// Scalar multiplication
 vector<double> scalarmult(vector<double> vec, float scalar)
 {
   int vecsize = vec.size();
@@ -26,8 +28,9 @@ vector<double> scalarmult(vector<double> vec, float scalar)
   return prod;
 }
 
-void read_file(vector<double>& masses, vector<vector<double> >& positions, 
-               vector<vector<double> >& velocities)
+// Read NBODYx fort.10 file for initial state
+void read_file(vector<double> &masses, vector<vector<double> > &positions, 
+               vector<vector<double> > &velocities)
 {
   ifstream inFile;
 
@@ -41,9 +44,20 @@ void read_file(vector<double>& masses, vector<vector<double> >& positions,
   int counter = 0;
   vector<double> current_position(3);
   vector<double> current_velocity(3);
-  while (inFile >> number){
-    if(counter == 7){counter = 0;}
 
+  // For all the floats in fort.10
+  while (inFile >> number){
+    /* Each particle gets 7 values
+         0 mass
+         1 position x
+         2 position y
+         3 position z
+         4 velocity x
+         5 velocity y
+         6 velocity z
+       Wrap through sets of 7 to assign values for individual stars */
+    
+    if(counter == 7){counter = 0;}
     if(counter == 0){masses.push_back(number);}
     else if(counter == 1){current_position[0] = number;}
     else if(counter == 2){current_position[1] = number;}
@@ -57,11 +71,10 @@ void read_file(vector<double>& masses, vector<vector<double> >& positions,
     }
     counter ++;
   }
-
 }
 
-vector<double> acc(vector<double>& masses, vector<vector<double> >& positions, 
-                   int index, vector<double>& position, double h, bool print)
+vector<double> acc(vector<double> &masses, vector<vector<double> > &positions, 
+                   int index, vector<double> &position, double h, bool print)
 {
   int N = masses.size();
   vector<double> current_acc(3);
@@ -89,49 +102,52 @@ vector<double> acc(vector<double>& masses, vector<vector<double> >& positions,
   return current_acc;
 }
 
-void rk4_evolve(vector<double>& masses, vector<vector<double> >& positions,
-                vector<vector<double> >& velocities, double h,
-                vector<vector<double> >& new_positions,
-                vector<vector<double> >& new_velocities,bool print)
+void rk4_step(vector<double>& masses, vector<vector<double> > &positions,
+              vector<vector<double> > &velocities, double h,
+              vector<vector<double> > &new_positions,
+              vector<vector<double> > &new_velocities,bool print)
 {
   int N = masses.size();
 
-  vector<double> k1(3), k2(3), k3(3), k4(3);
+  vector<double> k1(3), k2(3), k3(3);
   vector<double> k2_position(3),k3_position(3);
 
+  // For each particle
   for(int i=0; i<N; i++){
 
+    // Calculate k1
     k1 = scalarmult(acc(masses, positions, i, positions[i], h, print), h);
 
+    // Calculate k2
     k2_position = vectoradd(vectoradd(positions[i], 
                                       scalarmult(velocities[i], 0.5*h)),
                             scalarmult(k1, 0.125*h));
+    
     k2 = scalarmult(acc(masses, positions, i, k2_position, h, print), h);
 
+    // Calculate k3
     k3_position = vectoradd(vectoradd(positions[i],
                                       scalarmult(velocities[i], h)),
                             scalarmult(k2, 0.5*h));
+   
     k3 = scalarmult(acc(masses, positions, i, k3_position, h, print), h);
 
+    // Calculate the particle's new position
     new_positions[i] = 
       vectoradd(positions[i],
         scalarmult(vectoradd(velocities[i],
                              scalarmult(vectoradd(k1,scalarmult(k2,2.0)),
-                                        1.0/6.0)
-                             )
-                    , h)
-               );
+                                        1.0/6.0)), h));
 
+    // Calculate the particle's new velocity
     new_velocities[i] = 
       vectoradd(velocities[i],
                 vectoradd(scalarmult(k1,1.0/6.0),
                           vectoradd(scalarmult(k2,2.0/3.0),
-                                    scalarmult(k3,1.0/6.0))
-                         )
-               );
-
+                                    scalarmult(k3,1.0/6.0))));
   }
 
+  // Swap new and old positions and velocities
   std::swap(positions,new_positions);
   std::swap(velocities,new_velocities);
 }
